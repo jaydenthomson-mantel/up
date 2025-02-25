01aebf5fdc64becc6837a3c77483042f8f0b3acf
jaydenthomson <jayden.thomson@mantelgroup.com.au>
2025-02-25 21:19:38 +1000

    separate pagination logic and error handling into distinct files
    also add pagination method GetAllPages

---
+++ pagination_params.go
@@ -0,0 +1,27 @@
+package up
+
+import "strconv"
+
+type PaginationParams struct {
+	PageSize string
+}
+
+func (params PaginationParams) Validate() error {
+	pageSizeStr := params.PageSize
+	if pageSizeStr != "" {
+		pageSize, err := strconv.Atoi(pageSizeStr)
+		if err != nil || pageSize < 1 || pageSize > 100 {
+			return PageSizeError{BadPageSize: pageSizeStr}
+		}
+	}
+
+	return nil
+}
+
+func (params PaginationParams) ToMap() map[string]string {
+	m := make(map[string]string)
+	if params.PageSize != "" {
+		m["page[size]"] = params.PageSize
+	}
+	return m
+}
