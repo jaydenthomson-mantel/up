b018cca175837eb86f4152342bbcce30997b2763
jaydenthomson <jayden.thomson@mantelgroup.com.au>
2025-03-30 23:16:54 +1000

    refactor: streamline pagination handling by consolidating page retrieval logic

--- common/paged_data.go
+++ common/paged_data.go
@@ -17,7 +17,29 @@ type Getter interface {
 }
 
 func (page *PagedData[T]) GetNextPage(up Getter, token string) (*PagedData[T], error) {
-	url := page.Links.Next
+	return page.getPageFromURL(up, page.Links.Next, token)
+}
+
+func (page *PagedData[T]) GetAllPages(up Getter, token string) ([]*PagedData[T], error) {
+	pageList := []*PagedData[T]{page}
+	nextPage := page
+
+	for nextPage.Links.Next != "" {
+		var err error
+		nextPage, err = nextPage.getPageFromURL(up, nextPage.Links.Next, token)
+		if err != nil {
+			return nil, err
+		}
+		if nextPage == nil {
+			break
+		}
+		pageList = append(pageList, nextPage)
+	}
+
+	return pageList, nil
+}
+
+func (page *PagedData[T]) getPageFromURL(up Getter, url string, token string) (*PagedData[T], error) {
 	if url == "" {
 		return nil, nil
 	}
@@ -39,33 +61,4 @@ func (page *PagedData[T]) GetNextPage(up Getter, token string) (*PagedData[T], e
 		return &pagedData, nil
 	}
 	return nil, nil
-}
-
-func (page *PagedData[T]) GetAllPages(up Getter, token string) ([]*PagedData[T], error) {
-	nextPageUrl := page.Links.Next
-	pageList := []*PagedData[T]{page}
-
-	for nextPageUrl != "" {
-		resp, err := up.GetPaged(nextPageUrl, token)
-		if err != nil {
-			return nil, err
-		}
-
-		if rawData, ok := resp.(map[string]interface{}); ok {
-			jsonData, err := json.Marshal(rawData)
-			if err != nil {
-				return nil, err
-			}
-			var nextPage PagedData[T]
-			if err := json.Unmarshal(jsonData, &nextPage); err != nil {
-				return nil, err
-			}
-			pageList = append(pageList, &nextPage)
-			nextPageUrl = nextPage.Links.Next
-		} else {
-			break
-		}
-	}
-
-	return pageList, nil
 }
