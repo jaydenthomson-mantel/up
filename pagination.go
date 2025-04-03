6e0bf189766fb8e7e8479ec2f9fb0fc63ae3c847
jaydenthomson <jayden.thomson@mantelgroup.com.au>
2025-04-03 19:09:48 +1000

    keep all pagination together until it can go into its own package

--- pagination.go
+++ pagination.go
@@ -1,11 +1,26 @@
 package up
 
+import (
+	"fmt"
+	"strconv"
+)
+
+const maxPageSize = "100"
+
 type PagedData[T any] struct {
 	Data  []T `json:"data"`
 	Links struct {
 		Prev string `json:"prev"`
 		Next string `json:"next"`
 	} `json:"links"`
+}
+
+type PaginationParams struct {
+	PageSize string
+}
+
+type PageSizeError struct {
+	BadPageSize string
 }
 
 func (page *PagedData[T]) GetNextPage(up *UpClient, token string) (*PagedData[T], error) {
@@ -31,4 +46,28 @@ func (page *PagedData[T]) GetAllPages(up *UpClient, token string) ([]*PagedData[
 	}
 
 	return pageList, nil
+}
+
+func (params PaginationParams) Validate() error {
+	pageSizeStr := params.PageSize
+	if pageSizeStr != "" {
+		pageSize, err := strconv.Atoi(pageSizeStr)
+		if err != nil || pageSize < 1 || pageSize > 100 {
+			return PageSizeError{BadPageSize: pageSizeStr}
+		}
+	}
+
+	return nil
+}
+
+func (params PaginationParams) ToMap() map[string]string {
+	m := make(map[string]string)
+	if params.PageSize != "" {
+		m["page[size]"] = params.PageSize
+	}
+	return m
+}
+
+func (p PageSizeError) Error() string {
+	return fmt.Sprintf("page size %v not allowed", p.BadPageSize)
 }
